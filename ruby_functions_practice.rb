d941167f0f18b00374913a2de818496cec172e16
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-09-11 15:16:33 +0100

    Done test_length_of_string

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -17,3 +17,8 @@ end
 def divide(first_number, second_number)
   return first_number / second_number
 end
+
+def length_of_string(my_string)
+  my_string = "A string of length 21"
+  return my_string.length
+end
